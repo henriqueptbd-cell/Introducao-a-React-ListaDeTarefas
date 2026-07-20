add01764ed3e3567bfd07d8d52ce7c272dc27cec
Henrique.Camargo <henriqueptbd@gmail.com>
2026-07-19 21:11:21 -0300

    fix: correções

--- server.js
+++ server.js
@@ -7,25 +7,25 @@ const middlewares = jsonServer.defaults();
 server.use(middlewares);
 
 server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE,OPTIONS");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE,OPTIONS");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
 
-  if (req.method === "OPTIONS") {
-    return res.sendStatus(200);
-  }
+    if (req.method === "OPTIONS") {
+        return res.sendStatus(200);
+    }
 
-  next();
+    next();
 });
 
 server.get("/", (_req, res) => {
-  res.json({
-    message: "JSON Server está rodando",
-    endpoints: ["/tarefas"],
-  });
+    res.json({
+        message: "JSON Server está rodando",
+        endpoints: ["/tarefas"],
+    });
 });
 
 server.use(router);
@@ -34,5 +34,5 @@ const port = Number(process.env.PORT ?? 3333);
 const host = "0.0.0.0";
 
 server.listen(port, host, () => {
-  console.log(`JSON Server running on http://${host}:${port}`);
+    console.log(`JSON Server running on http://${host}:${port}`);
 });
